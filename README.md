8e3d1d1a554a700688fdd35444c5316f444badb7
abednarik <elcorreodeale@gmail.com>
2021-01-03 18:57:07 -0300

    Added documentation and script completed

--- README.md
+++ README.md
@@ -2,10 +2,89 @@
 
 Script to backup a local directory to an S3 bucket.
 
-##
+*DISCLAIMER* This script was only tested in Ubuntu 18.04 with Python 3.6 and is not intended to be used in production.
+Notifications emails are only sent to localhost.
+
+## Requirements
+
+* [Python](https://www.python.org/downloads/)
+* [tar](https://www.gnu.org/software/tar/)
 
 ## Install
 
+* Download backup2s3 script, save it in your PATH and give it execution permisssions. 
 
+```shell
+$ wget https://raw.githubusercontent.com/abednarik/backup2s3/master/backup2s3.py -O /usr/local/bin/backup2s3.py
+$ chmod +x /usr/local/bin/backup2s3.py
+```
+
+* Install python modules with pip:
+
+```shell
+$ pip3 install boto3
+```
+
+* Install AWS cli
+
+```shell
+sudo apt-get install -y awscli
+```
+
+* Configure AWS credentials 
+
+```shell
+$ aws configure 
+AWS Access Key ID [None]: YOURKEYHERE
+AWS Secret Access Key [None]: YOURSECRETKEYHERE
+Default region name [None]: your-region
+```
+
+## Configuration
+
+- Create a configuration file in a convenient directory. For example
+
+```shell
+$ sudo mkdir /etc/backup2s3
+```
+
+- Create a configuration file with a descriptive name and place in the directory created above. in this case `apache2.cfg`
+
+```
+[backup2s3]
+local_path = /etc/apache2
+bucket = abednarik-backups
+retention = 7
+email = localhost
+```
+
+| Option        | Description                                        | 
+| ------------- |:--------------------------------------------------:| 
+| local_path    | path to directory to backup                        | 
+| bucket        | S3 bucket to store backupss                        | 
+| retention     | Numbers of days to keep backups                    |
+| email         | Email address to send notifications after each run |
+
+
+
+## Usage
+
+You can schedule a cronjob to execute daily backups backups
+
+### Example
+
+- Edit crontab using 
+
+```shell
+sudo crontab -e
+```
+
+- Add an entry for backup2s3 to run every day at midnight
+
+```shell
+0 0 * * *    /usr/local/bin/backup2s3.py  > /dev/null 2>&1
+```
+
+*NOTE* Make user user running this cron is able to wirte to */var/log/backup2s3.log*
 
 
--- backup2s3.py
+++ backup2s3.py
@@ -8,32 +8,48 @@ import time
 import configparser
 import traceback
 import subprocess
-from datetime import datetime
+from shutil import which
+import boto3
+from botocore.exceptions import ClientError
+import smtplib
 
-
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.INFO, 
+                    format="%(asctime)s - %(levelname)s - %(message)s",
+                    filename='/var/log/backup2s3.log',
+                    filemode='w')
+logging.getLogger("botocore").propagate = False
 LOGGER = logging.getLogger(__name__)
 
-# EXit status
+# Exit status
 NO_CONFIG_FILE=10
 CONFIG_OPTION_MISSING=11
 LOCAL_DIR_ERROR=12
 TAR_FILE_ERROR=13
+BINARY_MISSING=14
+S3_UPLOAD_FAILED=15
+S3_LIST_FAILED=16
+S3_POLICY=17
+SMTP_ERROR=18
 
+# Constants
+TAR_CMD = 'tar'
+EXTENSION = '.tar.gz'
 
 class Backup2S3:
 
     config_file = ''
     local_path = ''
-    remote_path = ''
-    retention = ''
+    bucket = ''
+    retention = 0
     log_file = ''
-
+    email = ''
 
     def __init__(self, config_file=config_file):
         self._load_config(config_file)
 
     def _load_config(self, config_file):
+        """ Load configuration options"""
+
         config = configparser.ConfigParser()
         if not os.path.exists(config_file):
             LOGGER.error("Configuration file {} not found.".format(config_file))
@@ -41,40 +57,132 @@ class Backup2S3:
         config.read(config_file)
         try: 
             self.local_path = config['backup2s3']['local_path']
-            self.remote_path = config['backup2s3']['remote_path']
+            self.bucket = config['backup2s3']['bucket']
             self.retention = config['backup2s3']['retention']
-            self.log_file = config['backup2s3']['log_file']
+            self.email = config['backup2s3']['email']
         except KeyError as e:
             LOGGER.error("{} option missing in configuration file. Please see configuration example.".format(e))
             exit(CONFIG_OPTION_MISSING)        
 
-
     def _file_format(self, local_path):
-        extension = '.tar.gz'
+        """Set file name based on path and current date"""
 
         now = datetime.now()
         date_string = now.strftime("%Y%m%d%H%M%S")
-        file_name = "{}_{}{}".format(os.path.basename(local_path), date_string, extension)
-
+        file_name = "{}_{}{}".format(os.path.basename(local_path), date_string, EXTENSION)
         return file_name
 
 
     def _tmp_path(self, file_name):
+        """Simple helper function to set a tmp path to store a file locally"""
+
         tmp_dir = '/tmp/'
-        
         return tmp_dir + file_name
 
 
-    def upload_file(self, local_file_path):
-        pass
+    def _check_cmd(self, cmd):
+        if not which(cmd):
+            LOGGER.error("Binary {} not found in PATH".format(TAR_CMD))
+            exit(BINARY_MISSING)
+        return True
 
-    def cleanup_old_file():
-        pass
 
-    def sent_notification():
-        pass
+    def create_file(self, local_file_path):
+        """Create local archive"""
 
+        self._check_cmd(TAR_CMD)
+        try:
+            cmd = [TAR_CMD, 'czf', local_file_path, "-P", self.local_path]
+            subprocess.check_output(cmd).decode("utf-8").strip() 
+        except Exception as e: 
+            msg = "Error while creating tar file {}".format(e)      
+            self.send_notification(msg, TAR_FILE_ERROR)
     
+        LOGGER.info("Local Archive created succesfully in {}".format(local_file_path))
+
+        return True
+
+
+    def upload_file(self, local_file_path, file_name):
+        """Upload a file to an S3 bucket"""
+
+        # Upload the file
+        s3_client = boto3.client('s3')
+        try:
+            response = s3_client.upload_file(local_file_path, self.bucket, file_name)
+        except ClientError as e:
+            msg = "Error while uploading file to S3 bucket {}".format(e)      
+            self.send_notification(msg, S3_UPLOAD_FAILED)
+
+        LOGGER.info("File {} succesfully upload to S3 bucket {}".format(file_name, self.bucket))
+        return True
+
+
+    def cleanup_old_file(self):
+        """Set a policy in bucket to remove old objects"""
+
+        session = boto3.Session()
+        s3 = session.resource('s3')
+        bucket_lifecycle_configuration = s3.BucketLifecycleConfiguration(self.bucket)
+        try:
+            bucket_lifecycle_configuration.put(
+                LifecycleConfiguration={
+                    'Rules': [
+                        {
+                            'Expiration': {
+                                'Days': 7
+                            },
+                            'ID': 'life cycle config for bucket objects',
+                            'Filter': {
+                                'Prefix': '/',
+                            },
+                            'Status': 'Enabled',
+                            'NoncurrentVersionExpiration': {
+                                'NoncurrentDays': 7
+                            },
+                            'AbortIncompleteMultipartUpload': {
+                                'DaysAfterInitiation': 7
+                            }
+                        },
+                    ]
+                }
+            )
+        except ClientError as e:
+            msg = "Unable to apply bucket policy {}".format(e)      
+            self.send_notification(msg, S3_POLICY)
+
+        LOGGER.info("Retention policy applied succesfully to S3 bucket {}".format(self.bucket))
+
+        return True
+
+
+    def send_notification(self, msg, exit_status=0):
+        """Send email notification once process complete"""
+
+        sender = 'backuop2s3'
+        receivers = [self.email]
+
+        message = """From: From backup2s3
+        To: To {}
+        Subject: Backup2S3 Report
+
+        {}
+        """.format(self.email, msg)
+
+        try:
+            smtpObj = smtplib.SMTP('localhost')
+            smtpObj.sendmail(sender, receivers, message)         
+            LOGGER.info("backup2s3 report email sent")
+        except (smtplib.SMTPException, ConnectionRefusedError) as e:
+            LOGGER.error("Failed to send email {}".format(e))
+            exit(SMTP_ERROR)
+        
+        if exit_status != 0:
+            LOGGER.error("{}".format(msg))
+            exit(exit_status)
+        
+        return True
+
     def start(self):
         if not os.path.exists(self.local_path):
             LOGGER.error("Local directory {} not found.".format(self.local_path))
@@ -83,14 +191,14 @@ class Backup2S3:
         file_name = self._file_format(self.local_path)
         local_file_path = self._tmp_path(file_name)
 
-        try:
-            cmd = ['tar', 'czf', local_file_path, "-P", self.local_path]
-            subprocess.check_output(cmd).decode("utf-8").strip() 
-        except Exception as e:       
-            LOGGER.error("Error while creating tar file {}".format(e))
-            exit(TAR_FILE_ERROR)
+        self.create_file(local_file_path)
+        if self.upload_file(local_file_path, file_name):
+            os.remove(local_file_path)
+        self.cleanup_old_file()
 
+        self.send_notification(msg="Backup completed succesfully")
 
+        return True
 
 
 if __name__ == "__main__":
@@ -109,6 +217,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    Backup2S3(dryrun=args.dryrun,config_file=args.config_file).start()
+    Backup2S3(config_file=args.config_file).start()
 
     exit(0)
